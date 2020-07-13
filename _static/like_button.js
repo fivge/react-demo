077866b674568b5cc56e03e77b43a869f662fa7b
fivge <luanxingtong@gmail.com>
2020-07-13 23:06:40 +0800

    feat(static): 重用组件；添加jsx

--- _static/like_button.js
+++ _static/like_button.js
@@ -10,16 +10,20 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return "You liked this.";
+      return "You liked comment number " + this.props.commentID;
     }
 
-    return e(
-      "button",
-      { onClick: () => this.setState({ liked: true }) },
-      "Like"
-    );
+    // 显示一个 "Like" <button>
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
   }
 }
 
-const domContainer = document.querySelector("#like_button_container");
-ReactDOM.render(e(LikeButton), domContainer);
+// const domContainer = document.querySelector("#like_button_container");
+// ReactDOM.render(e(LikeButton), domContainer);
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll(".like_button_container").forEach((domContainer) => {
+  // Read the comment ID from a data-* attribute.
+  const commentID = parseInt(domContainer.dataset.commentid, 10);
+  ReactDOM.render(e(LikeButton, { commentID: commentID }), domContainer);
+});
