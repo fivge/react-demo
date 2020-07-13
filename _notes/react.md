7186197918f9695d3092aa548fa7b7c600202220
fivge <luanxingtong@gmail.com>
2020-07-14 00:31:04 +0800

    feat: update tutorial
    1. 参数传递
    2. 组件交互
    3. 状态提升

---
+++ _notes/react.md
@@ -0,0 +1,92 @@
+## react
+
+#### JSX
+
+在 JSX 中你可以任意使用 JavaScript 表达式，只需要用一个大括号把表达式括起来。每一个 React 元素事实上都是一个 JavaScript 对象，你可以在你的程序中把它当保存在变量中或者作为参数传递。
+
+```js
+<h1>Shopping List for {this.props.name}</h1>
+```
+
+#### 事件绑定
+
+```js
+onClick={() => {
+          alert("clicked!" + this.props.value);
+        }}
+```
+
+因为 DOM 元素 <button> 是一个内置组件，因此其 onClick 属性在 React 中有特殊的含义。
+
+而对于用户自定义的组件来说，命名就可以由用户自己来定义了。我们给 Square 的 onClick 和 Board 的 handleClick 赋予任意的名称，代码依旧有效。在 React 中，有一个命名规范，通常会将代表事件的监听 prop 命名为 on[Event]，将处理事件的监听方法命名为 handle[Event] 这样的格式。
+
+#### 参数传递
+
+```js
+  render() {
+    return <button className="square" >{this.props.value}</button>;
+  }
+
+<Square value={i} />
+```
+
+当你遇到需要同时获取多个子组件数据，或者两个组件之间需要相互通讯的情况时，需要把子组件的 state 数据提升至其共同的父组件当中保存。之后父组件可以通过 props 将状态数据传递到子组件当中。这样应用当中所有组件的状态数据就能够更方便地同步共享了。
+
+> 组件内部
+
+```js
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
+   this.setState({ value: 'X' });
+
+{this.state.value}
+```
+
+可以通过在 React 组件的构造函数中设置 this.state 来初始化 state。this.state 应该被视为一个组件的私有属性。
+
+> 不可变性
+
+调用 .slice() 方法创建 squares 数组的一个副本，而不是直接在现有的数组上进行修改
+
+###### 简化复杂的功能
+
+不可变性使得复杂的特性更容易实现。
+
+###### 跟踪数据的改变
+
+如果直接修改数据，那么就很难跟踪到数据的改变。跟踪数据的改变需要可变对象可以与改变之前的版本进行对比，这样整个对象树都需要被遍历一次。
+
+跟踪不可变数据的变化相对来说就容易多了。如果发现对象变成了一个新对象，那么我们就可以说对象发生改变了。
+
+####
+
+###### 确定在 React 中何时重新渲染
+
+不可变性最主要的优势在于它可以帮助我们在 React 中创建 _pure components_。我们可以很轻松的确定不可变数据是否发生了改变，从而确定何时对组件进行重新渲染。
+
+#### 函数组件
+
+TODO
+
+<https://zh-hans.reactjs.org/tutorial/tutorial.html#function-components>
+
+---
+
+## JavaScript
+
+### 箭头函数
+
+箭头函数无 this 执行，this 向上查找
+
+### class
+
+在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
+
+### array
+
+调用 .slice() 方法创建 squares 数组的一个副本，而不是直接在现有的数组上进行修改
--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // value: null
+      // value: this.props.value
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.props.onClick();
+          // this.setState({ value: 'X' });
+        }}
+      >
+        {/* {this.state.value} */}
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+      // squares: ['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
+    };
+  }
+
+  handleClick = i => {
+    // this.state.squares[i] = 'X';
+
+    // const squares = this.state.squares;
+
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  };
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = "Next player: X";
+    const status = 'Next player: X';
 
     return (
       <div>
@@ -55,6 +92,24 @@ class Game extends React.Component {
   }
 }
 
+// class ShoppingList extends React.Component {
+//   render() {
+//     return (
+//       <div className="shopping-list">
+//         <h1>Shopping List for {this.props.name}</h1>
+//         <ul>
+//           <li>Instagram</li>
+//           <li>WhatsApp</li>
+//           <li>Oculus</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// 用法示例: <ShoppingList name="Mark" />
+// ReactDOM.render(<ShoppingList name="Mark" />, document.getElementById("root"));
+
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById('root'));
